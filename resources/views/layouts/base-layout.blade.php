c2fa61ae5fcb700f55a4d1ae3fed34aa81e84c67
Muhindo Mubaraka <30492534+mubahood@users.noreply.github.com>
2023-01-02 22:33:19 +0300

    romina

--- resources/views/layouts/base-layout.blade.php
+++ resources/views/layouts/base-layout.blade.php
@@ -3,37 +3,37 @@
 
 <head>
     <meta charset="utf-8">
-    <title>Silicon | Online Courses Landing</title>
-    <base href="{{ url("") }}/" >
+    <title>IUIU - Alumni Association</title>
+    <base href="{{ url('') }}/">
 
 
     <!-- SEO Meta Tags -->
-    <meta name="description" content="Silicon - Multipurpose Technology Bootstrap Template">
-    <meta name="keywords"
-        content="bootstrap, business, creative agency, mobile app showcase, saas, fintech, finance, online courses, software, medical, conference landing, services, e-commerce, shopping cart, multipurpose, shop, ui kit, marketing, seo, landing, blog, portfolio, html5, css3, javascript, gallery, slider, touch, creative">
-    <meta name="author" content="Createx Studio">
+    <meta name="description" content="IUIU Alumni Association">
+    <meta name="keywords" content="iuiuaa, iuiu alumni, iuiu alumni association, islamic university in uganda alumni">
+    <meta name="author" content="Muhindo Mubaraka - Mubahood">
 
     <!-- Viewport -->
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- Favicon and Touch Icons -->
-    <link rel="apple-touch-icon" sizes="180x180" href="{{ url("") }}/assets/favicon/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="{{ url("") }}/assets/favicon/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="{{ url("") }}/assets/favicon/favicon-16x16.png">
-    <link rel="manifest" href="{{ url("") }}/assets/favicon/site.webmanifest">
-    <link rel="mask-icon" href="{{ url("") }}/assets/favicon/safari-pinned-tab.svg" color="#6366f1">
-    <link rel="shortcut icon" href="{{ url("") }}/assets/favicon/favicon.ico">
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ url('') }}/assets/img/logo.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ url('') }}/assets/img/logo.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ url('') }}/assets/img/logo.png">
+    <link rel="manifest" href="{{ url('') }}/assets/favicon/site.webmanifest">
+    <link rel="mask-icon" href="{{ url('') }}/assets/img/logo.png" color="#6366f1">
+    <link rel="shortcut icon" href="{{ url('') }}/assets/img/logo.png">
     <meta name="msapplication-TileColor" content="#080032">
-    <meta name="msapplication-config" content="{{ url("") }}/assets/favicon/browserconfig.xml">
+    <meta name="msapplication-config" content="{{ url('') }}/assets/favicon/browserconfig.xml">
     <meta name="theme-color" content="#ffffff">
 
     <!-- Vendor Styles -->
-    <link rel="stylesheet" media="screen" href="{{ url("") }}/assets/vendor/boxicons/css/boxicons.min.css" />
-    <link rel="stylesheet" media="screen" href="{{ url("") }}/assets/vendor/swiper/swiper-bundle.min.css" />
-    <link rel="stylesheet" media="screen" href="{{ url("") }}/assets/vendor/lightgallery/css/lightgallery-bundle.min.css"/>
+    <link rel="stylesheet" media="screen" href="{{ url('') }}/assets/vendor/boxicons/css/boxicons.min.css" />
+    <link rel="stylesheet" media="screen" href="{{ url('') }}/assets/vendor/swiper/swiper-bundle.min.css" />
+    <link rel="stylesheet" media="screen"
+        href="{{ url('') }}/assets/vendor/lightgallery/css/lightgallery-bundle.min.css" />
 
     <!-- Main Theme Styles + Bootstrap -->
-    <link rel="stylesheet" media="screen" href="{{ url("") }}/assets/css/theme.min.css">
+    <link rel="stylesheet" media="screen" href="{{ url('') }}/assets/css/theme.min.css">
 
     <!-- Page loading styles -->
     <style>
@@ -158,7 +158,7 @@
             <div class="page-spinner"></div><span>Loading...</span>
         </div>
     </div>
-    
+
     @yield('base-content')
 
 
@@ -169,21 +169,21 @@
 
 
     <!-- Vendor Scripts -->
-    <script src="{{ url("") }}/assets/vendor/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="{{ url("") }}/assets/vendor/smooth-scroll/dist/smooth-scroll.polyfills.min.js"></script>
-    <script src="{{ url("") }}/assets/vendor/parallax-js/dist/parallax.min.js"></script>
-    <script src="{{ url("") }}/assets/vendor/rellax/rellax.min.js"></script>
-    <script src="{{ url("") }}/assets/vendor/swiper/swiper-bundle.min.js"></script>
-    <script src="{{ url("") }}/assets/vendor/lightgallery/lightgallery.min.js"></script>
-    <script src="{{ url("") }}/assets/vendor/lightgallery/plugins/video/lg-video.min.js"></script>
-    <script src="{{ url("") }}/assets/vendor/lightgallery/plugins/zoom/lg-zoom.min.js"></script>
-    <script src="{{ url("") }}/assets/vendor/lightgallery/plugins/fullscreen/lg-fullscreen.min.js"></script>
+    <script src="{{ url('') }}/assets/vendor/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="{{ url('') }}/assets/vendor/smooth-scroll/dist/smooth-scroll.polyfills.min.js"></script>
+    <script src="{{ url('') }}/assets/vendor/parallax-js/dist/parallax.min.js"></script>
+    <script src="{{ url('') }}/assets/vendor/rellax/rellax.min.js"></script>
+    <script src="{{ url('') }}/assets/vendor/swiper/swiper-bundle.min.js"></script>
+    <script src="{{ url('') }}/assets/vendor/lightgallery/lightgallery.min.js"></script>
+    <script src="{{ url('') }}/assets/vendor/lightgallery/plugins/video/lg-video.min.js"></script>
+    <script src="{{ url('') }}/assets/vendor/lightgallery/plugins/zoom/lg-zoom.min.js"></script>
+    <script src="{{ url('') }}/assets/vendor/lightgallery/plugins/fullscreen/lg-fullscreen.min.js"></script>
 
     <!-- Main Theme Script -->
-    <script src="{{ url("") }}/assets/js/theme.min.js"></script>
+    <script src="{{ url('') }}/assets/js/theme.min.js"></script>
 
- 
-        
+
+
 
 </body>
 
