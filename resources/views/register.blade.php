a506dc454ea5c84284ab0d59b2f0e13273814831
Muhindo Mubaraka <30492534+mubahood@users.noreply.github.com>
2023-01-02 22:59:36 +0300

    fs

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -17,6 +17,11 @@
                 <div class="container d-flex flex-wrap justify-content-center justify-content-xl-start h-100 pt-5">
                     <div class="w-100 align-self-end pt-1 pt-md-4 pb-4" style="max-width: 526px;">
                         <h1 class="text-center text-xl-start">Joining IUIU Alumni Association</h1>
+
+                        <p class="lead fs-sm text-dark mt-3 border border-primary p-3 bg-secondary rounded">
+                            Dear Respected colleague, thank you for your interest in the IUIU ALUMNI REGROUP. We are planning to create an alumni database to enhance ways of reaching out and supporting one another.
+                            Please fill out this form to help us get to know you better. The details you provide will only be used. 
+                        </p>
                         <p class="text-center text-xl-start pb-3 mb-3">Already registred an account? <a href="login">Login
                                 in here</a></p>
                         <form class="needs-validation" method="POST" action="{{ admin_url('auth/login') }}"  novalidate>
