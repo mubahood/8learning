53277518731890db06e430b5f6ea1c7b439b429f
Muhindo Mubaraka <30492534+mubahood@users.noreply.github.com>
2023-01-29 12:45:06 +0300

    team

--- resources/views/about-us.blade.php
+++ resources/views/about-us.blade.php
@@ -162,6 +162,7 @@
 
 
 
+
     <!-- Team -->
     <section class="container  my-md-3 ">
         <h1 class="h1 text-center pt-1 pb-3 mb-3 mb-lg-4">Our Leadership</h1>
@@ -292,6 +293,35 @@
             <div class="col">
                 <div class="card card-hover border-0 bg-transparent">
                     <div class="position-relative">
+                        <img src="assets/images/team/joseph.jpg" class="rounded-3" alt="Jerome Bell">
+                        <div
+                            class="card-img-overlay d-flex flex-column align-items-center justify-content-center rounded-3">
+                            <span class="position-absolute top-0 start-0 w-100 h-100 bg-dark opacity-25 rounded-3"></span>
+                            <div class="position-relative d-flex zindex-2">
+                                <a href="#" class="btn btn-icon btn-secondary btn-facebook btn-sm bg-white me-2">
+                                    <i class="bx bxl-facebook"></i>
+                                </a>
+                                <a href="#" class="btn btn-icon btn-secondary btn-twitter btn-sm bg-white me-2">
+                                    <i class="bx bxl-twitter"></i>
+                                </a>
+                                <a href="#" class="btn btn-icon btn-secondary btn-linkedin btn-sm bg-white">
+                                    <i class="bx bxl-linkedin"></i>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card-body text-center p-3">
+                        <h3 class="fs-lg fw-semibold pt-1 mb-2">Mr. Twine Joseph Muganga</h3>
+                        <p class="fs-sm mb-0">Executive Member</p>
+                    </div>
+                </div>
+            </div>
+
+
+            <!-- Item -->
+            <div class="col">
+                <div class="card card-hover border-0 bg-transparent">
+                    <div class="position-relative">
                         <img src="assets/images/team/faiza.jpg" class="rounded-3" alt="Jerome Bell">
                         <div
                             class="card-img-overlay d-flex flex-column align-items-center justify-content-center rounded-3">
@@ -347,11 +377,152 @@
                     </div>
                 </div>
             </div>
+            <!-- Item -->
+            <div class="col">
+                <div class="card card-hover border-0 bg-transparent">
+                    <div class="position-relative">
+                        <img src="assets/images/team/abdulrahman.jpg" class="rounded-3">
+                        <div
+                            class="card-img-overlay d-flex flex-column align-items-center justify-content-center rounded-3">
+                            <span class="position-absolute top-0 start-0 w-100 h-100 bg-dark opacity-25 rounded-3"></span>
+                            <div class="position-relative d-flex zindex-2">
+                                <a href="#" class="btn btn-icon btn-secondary btn-facebook btn-sm bg-white me-2">
+                                    <i class="bx bxl-facebook"></i>
+                                </a>
+                                <a href="#" class="btn btn-icon btn-secondary btn-twitter btn-sm bg-white me-2">
+                                    <i class="bx bxl-twitter"></i>
+                                </a>
+                                <a href="#" class="btn btn-icon btn-secondary btn-linkedin btn-sm bg-white">
+                                    <i class="bx bxl-linkedin"></i>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card-body text-center p-3">
+                        <h3 class="fs-lg fw-semibold pt-1 mb-2">Mr. Abdulrahman</h3>
+                        <p class="fs-sm mb-0">Executive Member</p>
+                    </div>
+                </div>
+            </div>
+
+
+            <!-- Item -->
+            <div class="col">
+                <div class="card card-hover border-0 bg-transparent">
+                    <div class="position-relative">
+                        <img src="assets/images/team/buwembo.jpg" class="rounded-3">
+                        <div
+                            class="card-img-overlay d-flex flex-column align-items-center justify-content-center rounded-3">
+                            <span class="position-absolute top-0 start-0 w-100 h-100 bg-dark opacity-25 rounded-3"></span>
+                            <div class="position-relative d-flex zindex-2">
+                                <a href="#" class="btn btn-icon btn-secondary btn-facebook btn-sm bg-white me-2">
+                                    <i class="bx bxl-facebook"></i>
+                                </a>
+                                <a href="#" class="btn btn-icon btn-secondary btn-twitter btn-sm bg-white me-2">
+                                    <i class="bx bxl-twitter"></i>
+                                </a>
+                                <a href="#" class="btn btn-icon btn-secondary btn-linkedin btn-sm bg-white">
+                                    <i class="bx bxl-linkedin"></i>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card-body text-center p-3">
+                        <h3 class="fs-lg fw-semibold pt-1 mb-2">Mr. Buwembo Ali</h3>
+                        <p class="fs-sm mb-0">Executive Member</p>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Item -->
+            <div class="col">
+                <div class="card card-hover border-0 bg-transparent">
+                    <div class="position-relative">
+                        <img src="assets/images/team/kassim.jpg" class="rounded-3">
+                        <div
+                            class="card-img-overlay d-flex flex-column align-items-center justify-content-center rounded-3">
+                            <span class="position-absolute top-0 start-0 w-100 h-100 bg-dark opacity-25 rounded-3"></span>
+                            <div class="position-relative d-flex zindex-2">
+                                <a href="#" class="btn btn-icon btn-secondary btn-facebook btn-sm bg-white me-2">
+                                    <i class="bx bxl-facebook"></i>
+                                </a>
+                                <a href="#" class="btn btn-icon btn-secondary btn-twitter btn-sm bg-white me-2">
+                                    <i class="bx bxl-twitter"></i>
+                                </a>
+                                <a href="#" class="btn btn-icon btn-secondary btn-linkedin btn-sm bg-white">
+                                    <i class="bx bxl-linkedin"></i>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card-body text-center p-3">
+                        <h3 class="fs-lg fw-semibold pt-1 mb-2">Mr. Kassim Kayira</h3>
+                        <p class="fs-sm mb-0">Executive Member</p>
+                    </div>
+                </div>
+
+            <!-- Item -->
+            <div class="col">
+                <div class="card card-hover border-0 bg-transparent">
+                    <div class="position-relative">
+                        <img src="assets/images/team/taufiq.jpg" class="rounded-3">
+                        <div
+                            class="card-img-overlay d-flex flex-column align-items-center justify-content-center rounded-3">
+                            <span class="position-absolute top-0 start-0 w-100 h-100 bg-dark opacity-25 rounded-3"></span>
+                            <div class="position-relative d-flex zindex-2">
+                                <a href="#" class="btn btn-icon btn-secondary btn-facebook btn-sm bg-white me-2">
+                                    <i class="bx bxl-facebook"></i>
+                                </a>
+                                <a href="#" class="btn btn-icon btn-secondary btn-twitter btn-sm bg-white me-2">
+                                    <i class="bx bxl-twitter"></i>
+                                </a>
+                                <a href="#" class="btn btn-icon btn-secondary btn-linkedin btn-sm bg-white">
+                                    <i class="bx bxl-linkedin"></i>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card-body text-center p-3">
+                        <h3 class="fs-lg fw-semibold pt-1 mb-2">Mr. Prof Taufiq</h3>
+                        <p class="fs-sm mb-0">Executive Member</p>
+                    </div>
+                </div>
+            </div>
+
+
+            <!-- Item -->
+            <div class="col">
+                <div class="card card-hover border-0 bg-transparent">
+                    <div class="position-relative">
+                        <img src="assets/images/team/taufiq.jpg" class="rounded-3">
+                        <div
+                            class="card-img-overlay d-flex flex-column align-items-center justify-content-center rounded-3">
+                            <span class="position-absolute top-0 start-0 w-100 h-100 bg-dark opacity-25 rounded-3"></span>
+                            <div class="position-relative d-flex zindex-2">
+                                <a href="#" class="btn btn-icon btn-secondary btn-facebook btn-sm bg-white me-2">
+                                    <i class="bx bxl-facebook"></i>
+                                </a>
+                                <a href="#" class="btn btn-icon btn-secondary btn-twitter btn-sm bg-white me-2">
+                                    <i class="bx bxl-twitter"></i>
+                                </a>
+                                <a href="#" class="btn btn-icon btn-secondary btn-linkedin btn-sm bg-white">
+                                    <i class="bx bxl-linkedin"></i>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card-body text-center p-3">
+                        <h3 class="fs-lg fw-semibold pt-1 mb-2">Ms. Kayiira</h3>
+                        <p class="fs-sm mb-0">Executive Member</p>
+                    </div>
+                </div>
+            </div>
 
 
 
         </div>
     </section>
+
 
 
     <section class="container py-5 my-md-3 ">
