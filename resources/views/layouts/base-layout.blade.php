74ae7ff0b8f8ff2cb197a0d008bc83320eddb4e4
Muhindo Mubaraka <30492534+mubahood@users.noreply.github.com>
2024-09-09 11:27:16 +0300

    chore: Update base URL in base-layout.blade.php

--- resources/views/layouts/base-layout.blade.php
+++ resources/views/layouts/base-layout.blade.php
@@ -37,6 +37,22 @@
     <!-- Main Theme Styles + Bootstrap -->
     <link rel="stylesheet" media="screen" href="assets/css/theme.min.css">
 
+    <!--Start of Tawk.to Script-->
+    <script type="text/javascript">
+        var Tawk_API = Tawk_API || {},
+            Tawk_LoadStart = new Date();
+        (function() {
+            var s1 = document.createElement("script"),
+                s0 = document.getElementsByTagName("script")[0];
+            s1.async = true;
+            s1.src = 'https://embed.tawk.to/66dea94c50c10f7a00a61b32/1i7asalmi';
+            s1.charset = 'UTF-8';
+            s1.setAttribute('crossorigin', '*');
+            s0.parentNode.insertBefore(s1, s0);
+        })();
+    </script>
+    <!--End of Tawk.to Script-->
+
     <!-- Page loading styles -->
     <style>
         .page-loading {
